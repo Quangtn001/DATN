7f637421bfa1d129253fdf3ebd926790f1314e7e
Nguyễn Bá Quang <quangtn001@gmail.com>
2023-04-10 17:19:32 +0700

    update auth api

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const env = require("./config/envConfig");
+const connect = require("./config/db");
+const app = express();
+const userRoutes = require("./routes/userRoutes");
+
+// database connection
+connect();
+// add middleware
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ msg: "Welcome to DATN backend" });
+});
+
+// user routes
+app.use("/api", userRoutes);
+// app.use("/api", categoryRoutes);
+// app.use("/api", productRoutes);
+// app.use("/api", paymentRoutes);
+// app.use("/api", orderRoutes);
+
+const port = env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Your server is running at port number: ${port}`);
+});
